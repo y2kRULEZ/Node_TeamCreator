0af7d78bee10ffbebe841ce62807e9a0d016516a
y2kRULEZ <imanpace7@gmail.com>
2022-08-01 16:59:10 -0400

    edited card titles

--- src/managerHTML.js
+++ src/managerHTML.js
@@ -2,7 +2,7 @@ function managerHtml(manager){
     return `  <div class="col-sm-4 my-3">
     <div class="card">
       <div class="card-body">
-        <h5 class="card-title">Manger</h5>
+        <h5 class="card-title">Manager</h5>
         <ul class="list-group">
           <li class="list-group-item">name: ${manager.name}</li>
           <li class="list-group-item">id:  ${manager.id}</li>
